83cc61e4d98028c67dd7f97554142a2d0f9289b5
“jother043” <josemgh166@gmail.com>
2023-11-07 22:18:57 +0100

    Primeros pasos implementados con exito.

--- src/main/java/org/videojuego/Controladores/VideoJuegoBBDD.java
+++ src/main/java/org/videojuego/Controladores/VideoJuegoBBDD.java
@@ -2,14 +2,12 @@ package org.videojuego.Controladores;
 
 import org.videojuego.Connect.DataBaseConnection;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class VideoJuegoBBDD {
 
-    public Connection getConnection() throws SQLException {
+    public static Connection getConnection() throws SQLException {
         DataBaseConnection connection;
         connection = DataBaseConnection.getInstance();
         return connection.getConexion();
@@ -56,57 +54,91 @@ public class VideoJuegoBBDD {
         System.out.println("Usuario introducida correctamente...");
     }
 
-    public String seleccionarObservacionesUsuarios(String userName) throws SQLException {
+    /**
+     * Método que devuelve un string con todas las observaciones de un usuario en concreto, pasandole el idUsuario.
+     * tambien necesitamos el nombre del videojuego, por lo que necesitamos hacer un join con la tabla videojuego.
+     * y el nombre del usuario, por lo que necesitamos hacer un join con la tabla usuarios.
+     * @param idUsuario
+     * @return
+     * @throws SQLException
+     */
+    public String listarObservacionesUsuario(int idUsuario ) throws SQLException {
 
         Connection con = getConnection();
-        int idUsuario;
         StringBuilder sb = new StringBuilder();
-        String id = null;
-        String idVideoJuego = null;
-        String duracion = null;
-        String puntuacion = null;
-        String vecesJugado = null;
-        String idUsuario1 = null;
-        String nombreJuego = null;
-
-        try (PreparedStatement userSelect = con.prepareStatement("SELECT idUsuarios FROM usuarios WHERE nombre = ?")) {
-            PreparedStatement observacionSelect = con.prepareStatement("SELECT * FROM observaciones WHERE idUsuario = ?");
-            PreparedStatement juegoSelect = con.prepareStatement("SELECT nombre FROM videojuego WHERE idVideoJuego = ?");
-            userSelect.setString(1, userName);
-            ResultSet rs = userSelect.executeQuery();
-            if(!rs.next()) {
-                System.err.println("Error al obtener el usuario, no existe.");
-            }else {
-                idUsuario = rs.getInt(1);
-                sb.append("Usuario: " + userName + ", ");
-                observacionSelect.setInt(1, idUsuario);
-                ResultSet rs2 = observacionSelect.executeQuery();
-                if (rs2.next()) {
-                    id = rs2.getString(1);
-                    sb.append("ID: " + id + ", ");
-                    idVideoJuego = rs2.getString(2);
-                    sb.append("ID videoJuego: " + idVideoJuego + ", ");
-                    duracion = rs2.getString(3);
-                    sb.append("Duración: " + duracion + ", ");
-                    puntuacion = rs2.getString(4);
-                    sb.append("Puntuación: " + puntuacion + ", ");
-                    vecesJugado = rs2.getString(5);
-                    sb.append("Veces Jugado: " + vecesJugado + ", ");
-                    idUsuario1 = rs2.getString(6);
-                    sb.append("ID usuario: " + idUsuario1 + ", ");
-                    juegoSelect.setString(1, idVideoJuego);
-                    ResultSet rs3 = juegoSelect.executeQuery();
-                    if (rs3.next()) {
-                        nombreJuego = rs3.getString(1);
-                        sb.append("Nombre juego: " + nombreJuego + ", ");
-                    }else {
-                        System.err.println("Error al obtener el nombre del juego, no existe.");
-                    }
-                }else{
-                    System.err.println("Error al obtener las observaciones del usuario, no existen.");
-                }
+        try (PreparedStatement listarObservaciones = con.prepareStatement("SELECT * FROM observaciones INNER JOIN videojuego ON observaciones.idVideoJuego = videojuego.idVideoJuego INNER JOIN usuarios ON observaciones.idUsuario = usuarios.idUsuarios WHERE idUsuario = ?")) {
+            listarObservaciones.setInt(1, idUsuario);
+            ResultSet rs = listarObservaciones.executeQuery();
+            while (rs.next()) {
+                sb.append("ID Observacion: ").append(rs.getInt(1)).append("\n");
+                sb.append("ID VideoJuego: ").append(rs.getInt("idVideojuego")).append("\n");
+                sb.append("Nombre VideoJuego: ").append(rs.getString("nombre")).append("\n");
+                sb.append("Duracion: ").append(rs.getDouble(3)).append("\n");
+                sb.append("Puntuacion: ").append(rs.getDouble(4)).append("\n");
+                sb.append("Veces Jugado: ").append(rs.getInt(5)).append("\n");
+                sb.append("ID Usuario: ").append(rs.getInt("idusuarios")).append("\n");
+                sb.append("Nombre Usuario: ").append(rs.getString("nombreUsuario")).append("\n");
+                sb.append("Apellido Usuario: ").append(rs.getString("apellidos")).append("\n");
+                sb.append("--------------------------------------------------").append("\n");
             }
         }
         return sb.toString();
+    }
+
+    public static String listarJuegos() throws SQLException {
+
+        Connection con = getConnection();
+        StringBuilder sb = new StringBuilder();
+        try (PreparedStatement listarJuegos = con.prepareStatement("SELECT * FROM videojuego")) {
+            ResultSet rs = listarJuegos.executeQuery();
+            while (rs.next()) {
+                sb.append("ID VideoJuego: ").append(rs.getInt(1)).append("\n");
+                sb.append("Nombre VideoJuego: ").append(rs.getString(2)).append("\n");
+                sb.append("Genero: ").append(rs.getString(3)).append("\n");
+                sb.append("Plataforma: ").append(rs.getString(4)).append("\n");
+                sb.append("Pegi: ").append(rs.getInt(5)).append("\n");
+                sb.append("Precio: ").append(rs.getDouble(6)).append("\n");
+                sb.append("--------------------------------------------------").append("\n");
+            }
+        }
+        return sb.toString();
+    }
+
+    public boolean comprobarIdUsuario(int idUsuario) throws SQLException {
+        Connection con = getConnection();
+        boolean existe = false;
+        try (PreparedStatement userSelect = con.prepareStatement("SELECT idUsuarios FROM usuarios WHERE idUsuarios = ?")) {
+            userSelect.setInt(1, idUsuario);
+            ResultSet rs = userSelect.executeQuery();
+            if(rs.next()) {
+                existe = true;
+            }
+        }
+        return existe;
+    }
+
+    public boolean comprobarIdVideoJuego(int idVideoJuego) throws SQLException {
+        Connection con = getConnection();
+        boolean existe = false;
+        try (PreparedStatement userSelect = con.prepareStatement("SELECT idVideoJuego FROM videojuego WHERE idVideoJuego = ?")) {
+            userSelect.setInt(1, idVideoJuego);
+            ResultSet rs = userSelect.executeQuery();
+            if(rs.next()) {
+                existe = true;
+            }
+        }
+        return existe;
+    }
+    public static ArrayList<Usuario> listarUsuarios() throws SQLException {
+        Connection con = DataBaseConnection.getInstance().getConexion();
+        ArrayList<Usuario> usuarios = new ArrayList<>();
+        try (PreparedStatement userSelect = con.prepareStatement("SELECT * FROM usuarios")) {
+            ResultSet rs = userSelect.executeQuery();
+            while (rs.next()) {
+                Usuario usuario = new Usuario(rs.getInt(1), rs.getString(2), rs.getString(3));
+                usuarios.add(usuario);
+            }
+        }
+        return usuarios;
     }
 }
--- src/main/java/org/videojuego/Main.java
+++ src/main/java/org/videojuego/Main.java
@@ -4,7 +4,11 @@ import org.videojuego.Controladores.Observaciones;
 import org.videojuego.Controladores.Usuario;
 import org.videojuego.Controladores.VideoJuego;
 import org.videojuego.Controladores.VideoJuegoBBDD;
+
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -14,13 +18,14 @@ public class Main {
 
     private static VideoJuegoBBDD videoJuegoBBDD = new VideoJuegoBBDD();
     private static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         int opcion = 0;
         do {
             System.out.println(menu());
             opcion = sc.nextInt();
-            switch (opcion){
+            switch (opcion) {
                 case 1:
                     try {
                         System.out.println("Introduce el id del videojuego");
@@ -35,6 +40,7 @@ public class Main {
                         int pegi = sc.nextInt();
                         System.out.println("Introduce el precio del videojuego");
                         double precio = sc.nextDouble();
+                        validarDatosVideojuego(idVideoJuego, nombre, genero, plataforma, pegi, precio);
                         VideoJuego videoJuego = new VideoJuego(idVideoJuego, nombre, genero, plataforma, pegi, precio);
                         videoJuegoBBDD.insertarJuego(videoJuego);
                     } catch (SQLException | InputMismatchException o) {
@@ -50,6 +56,7 @@ public class Main {
                         String nombre = sc.next();
                         System.out.println("Introduce el apellido del usuario");
                         String apellido = sc.next();
+                        validarUsuario(idUsuario, nombre, apellido);
                         Usuario usuario = new Usuario(idUsuario, nombre, apellido);
                         videoJuegoBBDD.insertarUsuarios(usuario);
                     } catch (SQLException | InputMismatchException o) {
@@ -71,6 +78,7 @@ public class Main {
                         int vecesJugado = sc.nextInt();
                         System.out.println("Introduce el id del usuario");
                         int idUsuario = sc.nextInt();
+                        validarObservaciones(idObservaciones, idVideoJuego, duracion, puntuacion, vecesJugado, idUsuario);
                         Observaciones observaciones1 = new Observaciones(idObservaciones, idVideoJuego, duracion, puntuacion, vecesJugado, idUsuario);
                         videoJuegoBBDD.insertarObservaciones(observaciones1);
                     } catch (SQLException | InputMismatchException o) {
@@ -79,32 +87,151 @@ public class Main {
                     }
                     break;
                 case 4:
+
                     try {
-                        System.out.println("Introduce el nombre del usuario");
-                        String nombre = sc.next();
-                        System.out.println(videoJuegoBBDD.seleccionarObservacionesUsuarios(nombre));
+                        for (Usuario usuario : VideoJuegoBBDD.listarUsuarios()) {
+                            System.out.println(usuario);
+                        }
+                        System.out.println("Introduce el id del usuario");
+                        int idUsuario = sc.nextInt();
+                        System.out.println(videoJuegoBBDD.listarObservacionesUsuario(idUsuario));
                     } catch (SQLException | InputMismatchException o) {
                         System.out.println("Error al seleccionar las observaciones");
                         sc.nextLine();
                     }
                     break;
                 case 5:
+                    try {
+                        System.out.println(VideoJuegoBBDD.listarJuegos());
+                    } catch (SQLException e) {
+                        System.err.println("Error al listar los juegos");
+                    }
+                    break;
+                case 6:
                     System.out.println("Hasta luego!");
                     break;
                 default:
                     System.out.println("Opción incorrecta");
             }
-        }while (opcion != 5);
+        } while (opcion != 6);
     }
 
-    public static String menu(){
+
+    public static String menu() {
         StringBuilder menu = new StringBuilder();
         menu.append("1. Insertar videojuego\n");
         menu.append("2. Insertar usuario\n");
         menu.append("3. Insertar observaciones\n");
         menu.append("4. Seleccionar observaciones de un usuario\n");
-        menu.append("5. Salir.\n");
+        menu.append("5. Listar juegos \n");
+        menu.append("6. Salir\n");
         menu.append("Introduce una opción: ");
         return menu.toString();
     }
+
+    /**
+     * Método que valida los datos del videojuego introducidos por el usuario, si son correctos devuelve true, si no false
+     * y muestra un mensaje de error por pantalla indicando el error que se ha producido, utilizamos los setters de la clase.
+     *
+     * @return true si los datos son correctos, false si no lo son
+     */
+    public static boolean validarDatosVideojuego(int idVideoJuego, String nombre, String genero, String plataforma, int pegi, double precio) {
+
+        boolean datosCorrectos = true;
+        String mensajeError = "";
+        if (idVideoJuego < 0) {
+            datosCorrectos = false;
+            mensajeError += "El id del videojuego no puede ser negativo\n";
+        }
+        if (nombre == null || nombre.trim().equals("")) {
+            datosCorrectos = false;
+            mensajeError += "El nombre del videojuego no puede estar vacío\n";
+        }
+        if (genero == null || genero.trim().equals("")) {
+            datosCorrectos = false;
+            mensajeError += "El género del videojuego no puede estar vacío\n";
+        }
+        if (plataforma == null || plataforma.trim().equals("")) {
+            datosCorrectos = false;
+            mensajeError += "La plataforma del videojuego no puede estar vacía\n";
+        }
+        if (pegi < 0) {
+            datosCorrectos = false;
+            mensajeError += "El pegi del videojuego no puede ser negativo\n";
+        }
+        if (precio < 0) {
+            datosCorrectos = false;
+            mensajeError += "El precio del videojuego no puede ser negativo\n";
+        }
+        if (!datosCorrectos) {
+            System.out.println(mensajeError);
+        }
+        return datosCorrectos;
+    }
+
+    public static boolean validarUsuario(int idUsuario, String nombre, String apellido) {
+
+        boolean datosCorrectos = true;
+        String mensajeError = "";
+        if (idUsuario < 0) {
+            datosCorrectos = false;
+            mensajeError += "El id del usuario no puede ser negativo\n";
+        }
+        if (nombre == null || nombre.trim().equals("")) {
+            datosCorrectos = false;
+            mensajeError += "El nombre del usuario no puede estar vacío\n";
+        }
+        if (apellido == null || apellido.trim().equals("")) {
+            datosCorrectos = false;
+            mensajeError += "El apellido del usuario no puede estar vacío\n";
+        }
+        if (!datosCorrectos) {
+            System.out.println(mensajeError);
+        }
+        return datosCorrectos;
+    }
+
+    private static void validarObservaciones(int idObservaciones, int idVideoJuego, double duracion, double puntuacion, int vecesJugado, int idUsuario) throws SQLException {
+
+        boolean datosCorrectos = true;
+        String mensajeError = "";
+        if (idObservaciones < 0) {
+            datosCorrectos = false;
+            mensajeError += "El id de las observaciones no puede ser negativo\n";
+        }
+        if (idVideoJuego < 0 || !videoJuegoBBDD.comprobarIdVideoJuego(idVideoJuego)) {
+            if (!videoJuegoBBDD.comprobarIdVideoJuego(idVideoJuego)) {
+                mensajeError += "El id del videojuego no existe\n";
+            }
+            if (idVideoJuego < 0) {
+                mensajeError += "El id del videojuego no puede ser negativo\n";
+            }
+            datosCorrectos = false;
+        }
+        if (duracion < 0) {
+            datosCorrectos = false;
+            mensajeError += "La duración del juego no puede ser negativa\n";
+        }
+        if (puntuacion < 0) {
+            datosCorrectos = false;
+            mensajeError += "La puntuación del juego no puede ser negativa\n";
+        }
+        if (vecesJugado < 0) {
+            datosCorrectos = false;
+            mensajeError += "Las veces que se ha jugado no puede ser negativa\n";
+        }
+        if (idUsuario < 0 || !videoJuegoBBDD.comprobarIdUsuario(idUsuario)) {
+            if (!videoJuegoBBDD.comprobarIdUsuario(idUsuario)) {
+                mensajeError += "El id del usuario no existe\n";
+            }
+            if (idUsuario < 0) {
+                mensajeError += "El id del usuario no puede ser negativo\n";
+            }
+            datosCorrectos = false;
+        }
+        if (!datosCorrectos) {
+            System.out.println(mensajeError);
+        }
+    }
+
 }
